032a24a4ed15f5e364e32936d7e9c2853da0f983
sch <robert7kim7@gmail.com>
2018-12-06 04:51:47 -0800

    gray bg for home

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -7,7 +7,7 @@ export default class Home extends Component {
 
     render() {
         return (
-            <div className="rightContainer">
+            <div className="gray-bg">
                 <span id="star" class="oi oi-star"></span>
                 <h1>What is this website about?</h1>
                 <p>Discover the "Top Stuff" to your heart's content, anything from popping music to scandalous news to delicious restaurants. </p>
