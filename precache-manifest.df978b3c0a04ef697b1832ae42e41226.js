35dfa2fd2684efc63f7b707512ad01973b89724c
joesyoon <joesyoon@uw.edu>
2018-12-07 21:29:37 -0800

    Updates

---
+++ precache-manifest.df978b3c0a04ef697b1832ae42e41226.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "38526747da50192b7cf2edef6535b8a7",
+    "url": "/static/media/dawg.38526747.png"
+  },
+  {
+    "revision": "d4bee28ed5fe2105fba5",
+    "url": "/static/css/main.36ce909e.chunk.css"
+  },
+  {
+    "revision": "e3bf53713b8e4b3e3390",
+    "url": "/static/js/1.e3bf5371.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "d4bee28ed5fe2105fba5",
+    "url": "/static/js/main.d4bee28e.chunk.js"
+  },
+  {
+    "revision": "5918247937df92f516a96f5fae6e0b01",
+    "url": "/static/media/joeppa.59182479.jpg"
+  },
+  {
+    "revision": "2efce531d6164042f2a17e0ba1bb904b",
+    "url": "/static/media/rob.2efce531.jpg"
+  },
+  {
+    "revision": "74a1167e4a87e0ad54d62d11c370b722",
+    "url": "/static/media/kishore.74a1167e.jpg"
+  },
+  {
+    "revision": "59a73f1bdcd83c4f0ecee21a91d6c3ee",
+    "url": "/static/media/dav.59a73f1b.jpg"
+  },
+  {
+    "revision": "c29c26aa932f4ab5e6935ceecba6ac24",
+    "url": "/index.html"
+  }
+];
