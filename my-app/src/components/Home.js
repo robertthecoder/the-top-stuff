bf727632e07525ceb8ee8052b0f466021e51220e
sch <robert7kim7@gmail.com>
2018-12-07 16:32:17 -0800

    saved notes container

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -8,12 +8,46 @@ export default class Home extends Component {
     render() {
         return (
             <div className="gray-bg">
-                <span id="star" class="oi oi-star"></span>
-                <h1>What is this website about?</h1>
-                <p>Discover the "Top Stuff" to your heart's content, anything from popping music to scandalous news to delicious restaurants. </p>
-                <p>When you discover something, you can add it to the the notebook on the left. </p>
 
-                <h4>Recent Saved Notes:</h4>
+                <div className="home-intro">
+                    <span id="star" className="oi oi-star"></span>
+                    <h1>What is this Website About?</h1>
+
+                    <div >
+                        <p>Discover the "Top Stuff" to your heart's content, anything from popping music to scandalous news to delicious restaurants. </p>
+                        <p>When you discover something, you can add it to the the notebook on the left. </p>
+                    </div>
+
+
+
+                </div>
+
+                <div className="saved-notes-container">
+                    <h4>Recent Saved Notes:</h4>
+
+                    <div className="flex-cont">
+                        <div className="rend-note">
+                            <h5>
+                                Title
+                        </h5>
+                            <p>Body</p>
+                        </div>
+                        <div className="rend-note">
+                            <h5>
+                                Title
+                        </h5>
+                            <p>Body</p>
+                        </div>
+                        <div className="rend-note">
+                            <h5>
+                                Title
+                        </h5>
+                            <p>Body</p>
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
         );
     }
--- my-app/src/components/sidebar.js
+++ my-app/src/components/sidebar.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import Notepad from './Notepad'
 
+import firebase from 'firebase/app';
+
 export default class SideBar extends Component {
 
     constructor(props) {
